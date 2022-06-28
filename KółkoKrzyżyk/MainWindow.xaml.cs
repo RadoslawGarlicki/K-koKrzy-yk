1608bb097929712620c44c5ed5ee53e9d2de5210
RadoslawGarlicki <100523039+RadoslawGarlicki@users.noreply.github.com>
2022-06-28 09:45:56 +0200

    Initial commit

---
+++ KółkoKrzyżyk/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KółkoKrzyżyk
+{
+    public partial class MainWindow : Window
+    {
+        public bool IsPlayer1Turn { get; set; }
+        public int Counter { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        public void NewGame()
+        {
+            IsPlayer1Turn = false;
+            Counter = 0;
+
+            Button_0_0.Content = string.Empty;
+            Button_1_0.Content = string.Empty;
+            Button_2_0.Content = string.Empty;
+            Button_0_1.Content = string.Empty;
+            Button_1_1.Content = string.Empty;
+            Button_2_1.Content = string.Empty;
+            Button_0_2.Content = string.Empty;
+            Button_1_2.Content = string.Empty;
+            Button_2_2.Content = string.Empty;
+
+            Button_0_0.Background = Brushes.White;
+            Button_1_0.Background = Brushes.White;
+            Button_2_0.Background = Brushes.White;
+            Button_0_1.Background = Brushes.White;
+            Button_1_1.Background = Brushes.White;
+            Button_2_1.Background = Brushes.White;
+            Button_0_2.Background = Brushes.White;
+            Button_1_2.Background = Brushes.White;
+            Button_2_2.Background = Brushes.White;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsPlayer1Turn ^= true;
+            Counter++;
+
+            if (Counter > 9)
+            {
+                NewGame();
+                return;
+            }
+
+            var button = sender as Button;
+
+            button.Content = IsPlayer1Turn ? "X" : "O";
+
+            if (CheckIfPlayerWon())
+            {
+                Counter = 9;
+            }
+        }
+
+        private bool CheckIfPlayerWon()
+        {
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_0_1.Content && Button_0_1.Content == Button_0_2.Content)
+            {
+                Button_0_0.Background = Brushes.LightGreen;
+                Button_0_1.Background = Brushes.LightGreen;
+                Button_0_2.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_1_0.Content.ToString() != string.Empty && Button_1_0.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content)
+            {
+                Button_1_0.Background = Brushes.LightGreen;
+                Button_1_1.Background = Brushes.LightGreen;
+                Button_1_2.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_2_0.Content.ToString() != string.Empty && Button_2_0.Content == Button_2_1.Content && Button_2_1.Content == Button_2_2.Content)
+            {
+                Button_2_0.Background = Brushes.LightGreen;
+                Button_2_1.Background = Brushes.LightGreen;
+                Button_2_2.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_0.Content && Button_1_0.Content == Button_2_0.Content)
+            {
+                Button_0_0.Background = Brushes.LightGreen;
+                Button_1_0.Background = Brushes.LightGreen;
+                Button_2_0.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_0_1.Content.ToString() != string.Empty && Button_0_1.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content)
+            {
+                Button_0_1.Background = Brushes.LightGreen;
+                Button_1_1.Background = Brushes.LightGreen;
+                Button_2_1.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_2.Content && Button_1_2.Content == Button_2_2.Content)
+            {
+                Button_0_2.Background = Brushes.LightGreen;
+                Button_1_2.Background = Brushes.LightGreen;
+                Button_2_2.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_1.Content && Button_1_1.Content == Button_2_2.Content)
+            {
+                Button_0_0.Background = Brushes.LightGreen;
+                Button_1_1.Background = Brushes.LightGreen;
+                Button_2_2.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_1.Content && Button_1_1.Content == Button_2_0.Content)
+            {
+                Button_0_2.Background = Brushes.LightGreen;
+                Button_1_1.Background = Brushes.LightGreen;
+                Button_2_0.Background = Brushes.LightGreen;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
